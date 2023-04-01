014b0ec90f9ff59336c988bc5403068adfc87484
Gustavo Silva Lobo <gustavo.lobo95@gmail.com>
2023-03-31 22:00:17 -0300

    Ajustes de testes de repository

--- src/main/java/com/todo/todoapp/repository/UserRepository.java
+++ src/main/java/com/todo/todoapp/repository/UserRepository.java
@@ -1,16 +1,19 @@
 package com.todo.todoapp.repository;
 
 import com.todo.todoapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query("SELECT u from User u")
-    List<User> getAllUsers();
 
+    public List<User> findUsers();
+
+    public User findUserById(Long id);
+
+    public void deleteUserById(Long id);
 }
