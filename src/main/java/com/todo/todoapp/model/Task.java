c4373279353593ca744f8c49592684d0284714da
Gustavo Silva Lobo <gustavo.lobo95@gmail.com>
2023-05-06 17:37:42 -0300

    FEAT: Adding the task date atribute and the Jackson deserialization config.

--- src/main/java/com/todo/todoapp/model/Task.java
+++ src/main/java/com/todo/todoapp/model/Task.java
@@ -1,8 +1,15 @@
 package com.todo.todoapp.model;
 
-import com.fasterxml.jackson.annotation.*;
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,7 +23,6 @@ public class Task {
     private Long id;
     private String title;
     private String description;
-
     @ManyToOne
     @JsonIgnore
     @JoinTable(name = "user_task",
@@ -33,10 +39,16 @@ public class Task {
     )
     private User user;
 
-    public Task(String title, String description, User user) {
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @Column(name = "task_date")
+    private LocalDate date;
+
+    public Task(String title, String description, User user, LocalDate date) {
         this.title = title;
         this.description = description;
         this.user = user;
+        this.date = date;
     }
 
 }
