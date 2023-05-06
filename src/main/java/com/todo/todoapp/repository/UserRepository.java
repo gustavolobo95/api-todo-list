565384648c96ef043d6dfd9cc65ccd8032de2d1b
Gustavo Silva Lobo <gustavo.lobo95@gmail.com>
2023-05-06 17:38:04 -0300

    REFACTOR: Fixing some queries

--- src/main/java/com/todo/todoapp/repository/UserRepository.java
+++ src/main/java/com/todo/todoapp/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.todo.todoapp.repository;
 import com.todo.todoapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,10 +11,9 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-
-    public List<User> findUsers();
+    @Query("SELECT u FROM User u")
+    public List<User> findAllUsers();
 
     public User findUserById(Long id);
 
-    public void deleteUserById(Long id);
 }
